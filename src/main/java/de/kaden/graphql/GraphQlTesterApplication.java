5b3b0578c4484a86a29266084b94ba6285c84fdf
alex <it-architektur@alexander-kaden.de>
2019-06-21 13:16:44 +0200

    initial commit

---
+++ src/main/java/de/kaden/graphql/GraphQlTesterApplication.java
@@ -0,0 +1,13 @@
+package de.kaden.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlTesterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/kaden/graphql/Query.java
@@ -0,0 +1,7 @@
+package de.kaden.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+public class Query implements GraphQLQueryResolver{
+
+}
